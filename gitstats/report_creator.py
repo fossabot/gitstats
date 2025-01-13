4d5382589af0f3d253c017907704ab5e3c926e62
shenxianpeng <xianpeng.shen@gmail.com>
2025-01-13 19:27:05 +0200

    feat: support multi themes

--- gitstats/report_creator.py
+++ gitstats/report_creator.py
@@ -38,6 +38,7 @@ class HTMLReportCreator(ReportCreator):
         basedirs = [binarypath, secondarypath, "/usr/share/gitstats"]
         for file in (
             conf["style"],
+            "script.js",
             "sortable.js",
             "arrow-up.gif",
             "arrow-down.gif",
@@ -901,6 +902,12 @@ plot """
 	<script type="text/javascript" src="sortable.js"></script>
 </head>
 <body>
+
+<select id="theme">
+  <option value="default">White</option>
+  <option value="green">Green</option>
+</select>
+<script type="text/javascript" src="script.js"></script>
 """
             % (self.title, conf["style"], getversion)
         )
